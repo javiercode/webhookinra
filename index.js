8a11650280eb0f97035293a3cf0421bd91215183
javier.canqui <javier.canqui@inra.gob.bo>
2020-02-13 12:42:09 -0400

    correciones

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ restService.post("/chatbot", function(req, res) {
   var speech =req.body.queryResult.queryText;
   var ruta = "http://sinra.inra.gob.bo:8097/api/v1/extranet/movil/reporte/predio?nroTitulo="+req.body.queryResult.queryText;
 
-  request({
-      uri: RUTA,
-      json: true,
-  }).then(res=>{
-      speech = res.estado;
-  });
+  // request({
+  //     uri: RUTA,
+  //     json: true,
+  // }).then(res=>{
+  //     speech = res.estado;
+  // });
+
+    speech=ruta;
     // req.body.queryResult &&
     // req.body.queryResult.parameters &&
     // req.body.queryResult.parameters.echoText

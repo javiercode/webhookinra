b174455e6fcf6e7a3cd8f2a1bf8ca2f937b1e42a
javier.canqui <javier.canqui@inra.gob.bo>
2020-02-13 10:29:00 -0400

    correciones

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
+restService.post("/chatbot", function(req, res) {
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&

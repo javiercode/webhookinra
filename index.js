f9ad0a344658c05fcebe1443c7601764ff955b9e
javier.canqui <javier.canqui@inra.gob.bo>
2020-02-13 12:37:26 -0400

    correcciones

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request-promise");
 
 const restService = express();
+
 
 restService.use(
   bodyParser.urlencoded({
@@ -15,11 +17,20 @@ restService.use(bodyParser.json());
 
 restService.post("/chatbot", function(req, res) {
   var speech =req.body.queryResult.queryText;
+  var ruta = "http://sinra.inra.gob.bo:8097/api/v1/extranet/movil/reporte/predio?nroTitulo="+req.body.queryResult.queryText;
+
+  request({
+      uri: RUTA,
+      json: true,
+  }).then(res=>{
+      speech = res.estado;
+  });
     // req.body.queryResult &&
     // req.body.queryResult.parameters &&
     // req.body.queryResult.parameters.echoText
     //   ? req.body.queryResult.queryText
     //   : "Existe un problema: ."+req.body;
+
   return res.json({
 
   "fulfillmentText": speech,

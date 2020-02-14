5f2da225e1917ab32c1ddbcb76c3cd2e1a1859ce
javier.canqui <javier.canqui@inra.gob.bo>
2020-02-14 09:34:05 -0400

    correcion solicitud titulo

--- index.js
+++ index.js
@@ -16,26 +16,57 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/chatbot", function(req, res) {
-  var speech =req.body.queryResult.queryText;
-  // var ruta = "http://172.17.0.226:8097/api/v1/extranet/movil/reporte/predio?nroTitulo="+req.body.queryResult.queryText;
-  var ruta = "http://sinra.inra.gob.bo:8097/api/v1/extranet/movil/reporte/predio?nroTitulo="+req.body.queryResult.queryText;
+    var aNroTitulo=req.body.queryResult.queryText.split(':');
+    if(aNroTitulo.length==2){
+        var nroTitulo = aNroTitulo[1];
+        var speech =req.body.queryResult.queryText;
+        // var ruta = "http://172.17.0.226:8097/api/v1/extranet/movil/reporte/predio?nroTitulo="+nroTitulo;
+        var ruta = "http://sinra.inra.gob.bo:8097/api/v1/extranet/movil/reporte/predio?nroTitulo="+nroTitulo;
 
-  request({url:ruta,json:true},function (error, response, body) {
-      // console.log(body);
-      console.log(body['parcela']);
-      speech = body['parcela']?body['parcela']:'no tenemos informacion relacionada';
-      return res.json({
-          "fulfillmentText": speech,
-          "fulfillmentMessages": [
-              {
-                  "text": {
-                      "text": [speech]
-                  }
-              }
-          ],
-          "source": "<webhookinra>"
-      });
-  });
+
+        request({url:ruta,json:true},function (error, response, body) {
+            console.log(body);
+            console.log(body['parcela']);
+            var nroDocumento = body['numeroDocumento']?body['numeroDocumento']:'no tenemos informacion relacionada';
+
+            speech = body['parcela']?body['parcela']:'no tenemos informacion relacionada';
+
+            if(body['numeroDocumento']!=null){
+                var nombre = body['parcela']?body['parcela']:'S/N';
+                var superficie = body['supCodCat']?body['supCodCat']:'S/N';
+                var clasificacion = body['clasificacion']?body['clasificacion']:'S/N';
+                speech = 'NOMBRE: '+nombre+". SUPERFICIE: "+superficie+". CLASIFICACION: "+clasificacion;
+            }else{
+                speech = 'no tenemos informacion relacionada';
+            }
+
+            return res.json({
+                "fulfillmentText": speech,
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                            "text": [speech]
+                        }
+                    }
+                ],
+                "source": "<webhookinra>"
+            });
+        });
+
+    }else{
+        speech = 'No se puede buscar el titulo solicitado.';
+        return res.json({
+            "fulfillmentText": speech,
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [speech]
+                    }
+                }
+            ],
+            "source": "<webhookinra>"
+        });
+    }
 
 
   // request({

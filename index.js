f00cdc8ac73ebd9ef3a7e7229fd7e9ab7757eac8
javiercode <javier.azote@gmail.com>
2020-02-16 02:58:57 -0400

    implementacion hr

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ restService.use(bodyParser.json());
 restService.post("/chatbot", function(req, res) {
     var speech ='No se puede buscar el titulo solicitado.';
     var aQueryText=req.body.queryResult.queryText.split(':');
+    var aValidarHr=req.body.queryResult.queryText.split('/');
+
+    var respuesta = function (res, speech) {
+        return res.json({
+            "fulfillmentText": speech,
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [speech]
+                    }
+                }
+            ],
+            "source": "<webhookinra>"
+        });
+    };
 
     if(aQueryText.length==2){
         var parametro=req.body.queryResult.parameters.comodin;
@@ -38,7 +53,7 @@ restService.post("/chatbot", function(req, res) {
                     }else{
                         speech = body.mensajes[0].mensaje;
                     }
-                    return res.json({
+                    /*return res.json({
                         "fulfillmentText": speech,
                         "fulfillmentMessages": [
                             {
@@ -48,7 +63,8 @@ restService.post("/chatbot", function(req, res) {
                             }
                         ],
                         "source": "<webhookinra>"
-                    });
+                    });*/
+                    respuesta(res, speech);
                 });
                 break;
             case 'ci':
@@ -70,47 +86,44 @@ restService.post("/chatbot", function(req, res) {
                     }else{
                         speech = body.mensajes[0].mensaje;
                     }
-                    return res.json({
-                        "fulfillmentText": speech,
-                        "fulfillmentMessages": [
-                            {
-                                "text": {
-                                    "text": [speech]
-                                }
-                            }
-                        ],
-                        "source": "<webhookinra>"
-                    });
+                    respuesta(res, speech);
                 });
+                break;
+            case 'hr':
+                var hr = aQueryText[1];
+                hr= hr.trim().toUpperCase().replace('-','');
+                var aHr = hr.split('/');
+
+                if(aHr.length==2){
+                    var numero = aHr[0];
+                    var gestion= aHr[1];
+                    // var ruta = "http://172.17.0.226:8097/api/v1/extranet/movil/reporte/beneficiario?documentoIdentidad="+ci;
+                    var ruta = "http://sinra.inra.gob.bo:8104/api/sinadi/v1/hojaRuta/navegar?tipoDatoBusqueda=BNHG&pagina=0&cantidad=1&datoBusqueda=" +numero+"&datoBusqueda2="+gestion;
+
+                    request({url:ruta,json:true},function (error, response, body) {
+                        // console.log(body);
+                        // console.log(body['parcela']);
+                        if(body.listHojaRuta && body.listHojaRuta.length>0){
+                            var referencia = body['listHojaRuta'][0]['referencia']?body['listHojaRuta'][0]['referencia']:'S/N';
+                            speech = 'REFERENCIA: '+referencia;
+                        }else{
+                            speech = "No se encontró hojas de rutas con la información solicitada";
+                        }
+                        respuesta(res, speech);
+                    });
+                }else{
+                    speech ='El formato de la solicitud no es la correcta';
+                    respuesta(res, speech);
+                }
                 break;
             default:
                 speech ='No podemos procesar su solicitud';
-                return res.json({
-                    "fulfillmentText": speech,
-                    "fulfillmentMessages": [
-                        {
-                            "text": {
-                                "text": [speech]
-                            }
-                        }
-                    ],
-                    "source": "<webhookinra>"
-                });
+                respuesta(res, speech);
                 break;
         }
     }else{
         speech ='El formato de la solicitud no es la correcta';
-        return res.json({
-            "fulfillmentText": speech,
-            "fulfillmentMessages": [
-                {
-                    "text": {
-                        "text": [speech]
-                    }
-                }
-            ],
-            "source": "<webhookinra>"
-        });
+        respuesta(res, speech);
     }
 
 });
